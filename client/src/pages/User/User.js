ca8a845d87f41bc2eca8315e3b673b3d741b8442
Adam Madueno <amadueno17@yahoo.com>
2020-09-24 15:57:07 -0700

    added

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Col,Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import axios from 'axios'
 
 
@@ -15,17 +15,18 @@ const Character = () => {
         character: []
 
     })
-   
+
 
     characterState.handleInputChange = event => {
         setCharacterState({ ...characterState, [event.target.name]: event.target.value })
         console.log('hi')
-        
+
 
     }
     characterState.handleCreateCharacter = event => {
-        event.preventDefault ()
-        axios.post('/api/characters',{
+        event.preventDefault()
+
+        axios.post('/api/characters', {
             name: characterState.name,
             class: characterState.class,
             background: characterState.background,
@@ -33,90 +34,133 @@ const Character = () => {
             faction: characterState.faction,
             alignment: characterState.alignment,
             exp: characterState.exp,
-            
-
-
+        }, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('user')}`
+            }
         })
+            .then(({ data: character }) => {
+                setCharacterState({
+                    ...characterState, name: ' ',
+                    class: ' ',
+                    background: ' ',
+                    race: ' ',
+                    faction: ' ',
+                    alignment: ' ',
+                    exp: ' '
+                })
 
-        
 
-        
-        // send the req.body=characterState
+                    .catch(err => {
+                        console.error(err)
+
+                    })
+            })
+        }
+
+        return (
+            <>
+                <Form>
+                    <FormGroup row>
+                        <Label for="name" sm={2}>Name</Label>
+                        <Col sm={10}>
+                            <input type="name"
+                                name="name"
+                                value={characterState.name}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="class" sm={2}>Class</Label>
+                        <Col sm={10}>
+                            <input type="class"
+                                name="class"
+                                value={characterState.class}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="race" sm={2}>Race</Label>
+                        <Col sm={10}>
+                            <input type="race"
+                                name="race"
+                                value={characterState.race}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="faction" sm={2}>Faction</Label>
+                        <Col sm={10}>
+                            <input type="faction"
+                                name="faction"
+                                value={characterState.faction}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="alignment" sm={2}>Alignment</Label>
+                        <Col sm={10}>
+                            <input type="alignment"
+                                name="alignment"
+                                value={characterState.alignment}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="exp" sm={2}>Exp</Label>
+                        <Col sm={10}>
+                            <input type="exp"
+                                name="exp"
+                                value={characterState.exp}
+                                onChange={characterState.handleInputChange} />
+                        </Col>
+                    </FormGroup>
+
+                    <Col sm={{ size: 10, offset: 2 }}>
+                        <Button onClick={characterState.handleCreateCharacter}>Create Character</Button>
+                    </Col>
+
+                </Form>
+
+
+
+            </>
+
+
+
+
+        )
 
     }
-    
-    return (
-        <>
-          <Form>
-      <FormGroup row>
-        <Label for="name" sm={2}>Name</Label>
-        <Col sm={10}>
-         <input type="name"
-         name="name"
-         value={characterState.name}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="class" sm={2}>Class</Label>
-        <Col sm={10}>
-        <input type="class"
-         name="class"
-         value={characterState.class}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="race" sm={2}>Race</Label>
-        <Col sm={10}>
-        <input type="race"
-         name="race"
-         value={characterState.race}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="faction" sm={2}>Faction</Label>
-        <Col sm={10}>
-        <input type="faction"
-         name="faction"
-         value={characterState.faction}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="alignment" sm={2}>Alignment</Label>
-        <Col sm={10}>
-        <input type="alignment"
-         name="alignment"
-         value={characterState.alignment}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="exp" sm={2}>Exp</Label>
-        <Col sm={10}>
-        <input type="exp"
-         name="exp"
-         value={characterState.exp}
-         onChange={characterState.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      
-        <Col sm={{ size: 10, offset: 2 }}>
-          <Button onClick={characterState.handleCreateCharacter}>Create Character</Button>
-        </Col>
-      
-    </Form>
-  
 
-           
-        </>
+export default Character
+   // characterState.handleCreateCharacter = event => {
+        //     event.preventDefault ()
+        //     axios.post('/api/characters',{
+        //         name: characterState.name,
+        //         class: characterState.class,
+        //         background: characterState.background,
+        //         race: characterState.race,
+        //         faction: characterState.faction,
+        //         alignment: characterState.alignment,
+        //         exp: characterState.exp,
+
+
+
+        //     })
 
 
 
 
-    )
-    
-}   
-export default Character 
+        //     // send the req.body=characterState
+
+        // }
+// saveCharacter({
+                        //   text: document.getElementById('character').value,
+                        //   isDone: false
+                        // })
+                        // const characterElem = document.createElement('li')
+                    // characterElem.className = character.isDone ? 'list-group-item list-group-item-success' : 'list-group-item'
+                    // characterElem.textContent = character.text
+                    // document.getElementById('characters').append(characterElem)
+                    // document.getElementById('character').value = ''
