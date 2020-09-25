e978973bc85be7ddcbe57a2e76ea86d51fe219f2
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-25 12:02:48 -0700

    note function

--- client/src/pages/Notes/Notes.js
+++ client/src/pages/Notes/Notes.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Button, Card, CardTitle, CardText, CardImg, CardImgOverlay, Col, Container, Row } from 'reactstrap'
 import Moment from 'react-moment'
 
+// import Component from '../../components'
 
-
-class notes extends Component {
-
-render() {
+// class Notes extends Component {
+const Notes = () => {
+{
   return (
     <div>
       <Container>
